44a12e46e5c9636b8b34aae2d5f8db4bce336d46
Nikita-Devsarkar <devsarkarnikita@gmail.com>
2026-07-25 16:51:43 +0530

    Create login page UI with Tailwind CSS

--- bug-tracker-frontend/src/pages/Login.jsx
+++ bug-tracker-frontend/src/pages/Login.jsx
@@ -1,6 +1,21 @@
 function Login() {
     return (
-        <h1>Login Page</h1>
+       <div className="min-h-screen flex  flex-col items-center justify-center bg-gray-100 ">
+            <h1 className="text-4xl font-bold mb-6">Bug Tracker</h1>
+
+            <input 
+            type = "email" 
+            placeholder="Email"
+            className = "border border-gray-400 p-2 w-72 mb-4"/>
+
+            <input 
+            type = "password" 
+            placeholder="Password"
+            className = "border border-gray-400 p-2 w-72 mb-4"
+            />
+
+            <button className="bg-blue-600 text-white p-2 rounded w-72 hover:bg-blue-700">Login</button>
+        </div>
     )
 }
 
